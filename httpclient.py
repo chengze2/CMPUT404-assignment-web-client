17324c6d19560670a42315d8e34fc597070cfbc5
chengze <chengze2@ualberta.ca>
2019-02-07 16:21:19 -0700

    handle freetests

--- httpclient.py
+++ httpclient.py
@@ -23,6 +23,7 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+from urllib.parse import urlparse
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -33,7 +34,8 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port(self,url):
+        return 80
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,13 +43,19 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        data_list = data.split("\r\n")
+        code = data_list[0].split(" ")[1]
+        return int(code)
 
     def get_headers(self,data):
-        return None
+        data_list = data.split("\r\n\r\n")
+        header = data_list[0]
+        return header
 
     def get_body(self, data):
-        return None
+        data_list = data.split("\r\n\r\n")
+        body = data_list[1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,13 +76,99 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        url_components = urlparse(url)
+        scheme = url_components.scheme
+        hostname = url_components.hostname
+        port = url_components.port
+        path = url_components.path
+
+        if(port == None):
+            port = self.get_host_port(url)
+        if(path == ""):
+            path = "/"
+        '''
+        print("!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(scheme)
+        print(hostname)
+        print(port)
+        print(path)
+        '''
+        self.connect(hostname,port)
+
+        request_payload = "GET {path} HTTP/1.1\r\n".format(path=path)
+        request_payload += "Host: {host}:{port}\r\n\r\n".format(host=hostname,port=port)
+        
+        self.sendall(request_payload)
+        data = self.recvall(self.socket)
+
+        code = self.get_code(data)
+        #header = self.get_headers(data)
+        body = self.get_body(data)
+        '''
+        print("@@")
+        print(data)
+        print("@")
+        print(code)
+        print(body)
+        print("@@")
+
+        print('`````````````````')
+        print(request_payload)
+        print('`````````````````')
+        '''
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        url_components = urlparse(url)
+        scheme = url_components.scheme
+        hostname = url_components.hostname
+        port = url_components.port
+        path = url_components.path
+        '''
+        print("!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(scheme)
+        print(hostname)
+        print(port)
+        print(path)
+        '''
+        content = ""
+        try:
+            for item in args.items():
+                print(item)
+                content += str(item[0]) + "=" + str(item[1]) + "&"
+            content = content[:-1]
+        except:
+            pass
+
+        self.connect(hostname,port)
+
+        request_payload = "POST {path} HTTP/1.1\r\n".format(path=path)
+        request_payload += "Host: {host}:{port}/\r\n".format(host=hostname,port=port,path=path)
+        request_payload += "Content-Length: {length}\r\n\r\n".format(length=len(content))
+        request_payload += content + "\r\n"
+
+        self.sendall(request_payload)
+        data = self.recvall(self.socket)
+        '''
+        print("@")
+        print(data)
+        print("@")
+        '''
+        code = self.get_code(data)
+        #header = self.get_headers(data)
+        body = self.get_body(data)
+        '''
+        print('`````````````````')
+        print(path)
+        print(request_payload)
+        print(code)
+        print(body)
+        print('`````````````````')
+        '''
+        self.close()
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
